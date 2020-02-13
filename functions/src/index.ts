6fa560b8491712a05f2590639415a59faccca57f
Masatomi KINO <masatomix@ki-no.org>
2020-02-14 00:25:03 +0900

    Women's 側の追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,75 +1,58 @@
 import * as functions from 'firebase-functions'
-import OrchestratorApi from 'uipath-orchestrator-api-node'
 import request from 'request'
-import moment from 'moment'
+import moment from 'moment-timezone'
 
 const sendSlack = (message: string) => {
-    const option = {
-        url: 'https://hooks.slack.com/services/xxxxxxx',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        json: { text: message, channel: '#general' }
-    }
-    request(option)
-}
-
-const executeLogic = async () => {
-    const config = {
-        "userinfo": {
-            "tenancyName": "default",
-            "usernameOrEmailAddress": "admin",
-            "password": "xxxxxx"
-        },
-        "serverinfo": {
-            "servername": "https://www.example.com/"
-        }
-    }
-    // const config = {
-    //     "serverinfo": {
-    //         "servername": "https://platform.uipath.com/[AccountLogicalName]/[ServiceName]",
-    //         "refresh_token": "[User Key]",
-    //         "tenant_logical_name": "[Tenant Logical Name]",
-    //         "client_id": "[Client Id]]"
-    //     }
-    // }
-
-  const api = new OrchestratorApi(config)
-  // まずは認証
-  await api.authenticate()
-
-  const li = await api.license.find()
-  const allowed = li.Allowed
-  const used = li.Used
-
-  const dataStr = new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })
-  // 参考 http://watanabeyu.blogspot.com/2019/10/firebase-functionsdate9.html
-  const now = moment(dataStr)
-  let message: string = now.format('YYYY/MM/DD HH:mm')
-
-  // 上記で日本語日付を算出もしくは、環境変数にTZをを指定する
-  // https://thr3a.hatenablog.com/entry/20190417/1555510726
-
-  message += ` 時点の ${config.serverinfo.servername} のライセンス情報です。\n`
-  for (const prop in allowed) {
-    message += `${prop}: ${used[prop]} / ${allowed[prop]}\n`
+  const option = {
+    url: 'https://hooks.slack.com/services/xxx',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    json: { text: message },
   }
-  message += `AttendedConcurrent: ${li.AttendedConcurrent}\n`
-  message += `DevelopmentConcurrent: ${li.DevelopmentConcurrent}\n`
-  message += `StudioXConcurrent: ${li.StudioXConcurrent}\n`
-  sendSlack(message)
-  return li
+  request(option)
 }
 
-// export const checkLicense = functions.https.onRequest(async (req, response) => {
-//   const license = await executeLogic()
-//   response.json(license)
+const executeLogic = async (): Promise<string> => {
+  const friendsData = await executeRequest('https://api.doorkeeper.jp/groups/uipath-friends')
+  const womenData = await executeRequest('https://api.doorkeeper.jp/groups/uipath-friends-women')
+
+  const now = moment()
+  let message: string = ''
+
+  message += `${friendsData.group.name} のメンバ数: ${friendsData.group.members_count}\n`
+  message += `${womenData.group.name} のメンバ数: ${womenData.group.members_count}\n`
+  message += `${friendsData.group.public_url}\n`
+  message += `${womenData.group.public_url}\n`
+  message += `(${now.tz('Asia/Tokyo').format('YYYY/MM/DD HH:mm')} 時点)`
+  console.log(message)
+  sendSlack(message)
+  return message
+}
+
+// export const checkDoorkeeper = functions.https.onRequest(async (req, response) => {
+//   const message = await executeLogic()
+//   response.end(message)
 // })
 
-export const checkLicensePubSub = functions.pubsub
-  .schedule('0 */1 * * *')
+export const checkDoorkeeperPubSub = functions.pubsub
+  .schedule('0 18 * * *')
   .timeZone('Asia/Tokyo')
   .onRun(async context => {
     await executeLogic()
   })
+
+function executeRequest(url: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    request(url, (err, response, body) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      const obj = JSON.parse(body)
+      resolve(obj)
+      return
+    })
+  })
+}
